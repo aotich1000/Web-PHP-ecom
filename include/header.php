55e2ef5fd26e4a01863ad4ec91dc13baca145d01
Tamina15 <minhnhat66442658@gmail.com>
2022-05-24 21:43:25 +0700

    24/5 index.php

--- include/header.php
+++ include/header.php
@@ -1,16 +1,45 @@
-<div class="header">
-    <div class="logo">
-        <a href="index.php"><img src="./images/logo2.png" alt="logo"></a>
-    </div>
-    <div class="search-box">
-        <form action="index.php" method="GET">
-            <input type="text" name="search" id="sr-field" placeholder="Bạn muốn tìm gì...">
-            <button type="submit">
-                <p>Tìm Kiếm</p>
-            </button>
-        </form>
-    </div>
-    <div class="shop-cart">
-        <a href="index.php?id=shop-cart"><img src="./images/shop-cart.png" alt="shop-cart"></a>
+<div class="header container">
+    <div class="row justify-content-evenly">
+
+        <div class="logo col">
+            <a href="index.php"><img src="./images/logo2.png" alt="logo"></a>
+        </div>
+        <div class="search-box col-6">
+            <form action="index.php" method="GET">
+                <input type="text" name="search" id="sr-field" placeholder="Bạn muốn tìm gì...">
+                <button type="submit">
+                    <p>Tìm Kiếm</p>
+                </button>
+            </form>
+        </div>
+        <div class="col d-flex align-items-center">
+
+            <div class="shop-cart col-3">
+                <a href="index.php?id=shop-cart"><img src="./images/shop-cart.png" alt="shop-cart"></a>
+            </div>
+
+            <!-- <div class="log-field"> -->
+            <?php if (!isset($_SESSION['user'])) {
+            ?>
+                <div class=" col d-flex align-items-center justify-content-evenly">
+                    <a href="index.php?id=signin" class="btn btn-warning">Đăng nhập</a>
+                    <a class="btn btn-success" href="index.php?id=signup">Đăng ký</a>
+                </div>
+            <?php
+            } else if (isset($_SESSION['user'])) {
+                $data = $_SESSION['user']; ?>
+                <div>
+                    <div class="dropdown-toggle col-2" id="dropdownMenuButton" data-toggle="dropdown">
+                        <img src="images/icon-user.png" alt="anhdaidien" class="user-icon">
+                        <?php echo $data['hoten'] ?>
+                    </div>
+                    <div class="dropdown">
+                        <a class="dropdown-item" href="index.php?id=quanlytaikhoan">Quản lý tài khoản</a>.
+                        <a class="dropdown-item" href="index.php?id=out">Đăng xuất</a>
+                    </div>
+                </div>
+            <?php } ?>
+            <!-- </div> -->
+        </div>
     </div>
 </div>
--- include/menu.php
+++ include/menu.php
@@ -2,53 +2,54 @@
 $sql_loaisp = "SELECT * FROM tbl_phanloaisp ORDER BY id_loaisp ";
 $query = mysqli_query($con, $sql_loaisp);
 ?>
-
 <script>
-  function phanloai(loaisp, page) {
-    $.ajax({
-      url: "hienthi-sp-ajax-phanloai.php",
-      type: "GET",
-      cache: false,
-      data: {
-        page_no: page,
-        loaisp: loaisp
-      },
-      beforeSend: function() {
-        $("#overlay").show();
-        $("#main").hide();
-      },
-      success: function(response) {
-        $("#data").html(response);
-        setInterval(function() {
-          $("#overlay").hide();
-        }, 500);
-      }
-    });
-    $(document).on("click", ".page-link", function(e) {
-      e.preventDefault();
-      var pageId = $(this).attr("id");
-      phanloai(loaisp, pageId);
-    });
-  }
+    function phanloai(loaisp, page) {
+        $.ajax({
+            url: "hienthi-sp-ajax-phanloai.php",
+            type: "GET",
+            cache: false,
+            data: {
+                page_no: page,
+                loaisp: loaisp
+            },
+            beforeSend: function() {
+                $("#overlay").show();
+                $("#main").hide();
+            },
+            success: function(response) {
+                $("#data").html(response);
+                setInterval(function() {
+                    $("#overlay").hide();
+                }, 500);
+            }
+        });
+        $(document).on("click", ".page-link", function(e) {
+            e.preventDefault();
+            var pageId = $(this).attr("id");
+            phanloai(loaisp, pageId);
+        });
+    }
 </script>
-<div class="menu">
-  <ul>
-    <li class="item-menu"><a href="index.php">Trang chủ</a></li>
-    <li class="item-menu"> <a href=""> Phân loại </a>
-      <div class="dropdown">
-        <ul>
-          <?php
-          while ($row_title = mysqli_fetch_array($query)) { ?>
-            <li>
-              <a id="loaisp" onclick="phanloai(<?php echo $row_title['id_loaisp'] ?>,1)">
-                <?php echo $row_title['ten_loaisp']; ?>
-              </a>
-            </li>
-          <?php
-          }
-          ?>
-        </ul>
-      </div>
-    </li>
-  </ul>
+<div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="index.php">Trang chủ</a>
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Phân loại</a>
+                    <ul class="dropdown-menu">
+                        <?php
+                        while ($row_title = mysqli_fetch_array($query)) { ?>
+                            <li>
+                                <a class="dropdown-item" id="loaisp" onclick="phanloai(<?php echo $row_title['id_loaisp'] ?>,1)">
+                                    <?php echo $row_title['ten_loaisp']; ?>
+                                </a>
+                            </li>
+                        <?php
+                        }
+                        ?>
+                    </ul>
+                </li>
+        </div>
+    </nav>
 </div>
