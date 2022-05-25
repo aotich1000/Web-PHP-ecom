a8e42b8229de863b353e183a0076600962912772
Ai-chan <72846857+aotich1000@users.noreply.github.com>
2022-05-25 11:08:08 +0700

    Revert "Merge branch 'master' of"
    This reverts commit 6e05cc9d36b053fe7a1a18ead6cc8195fbff1902, reversing
    changes made to 8a47814a7d4548ebfe9f45f9877638820838f6a0.

--- include/topbar.php
+++ include/topbar.php
@@ -1,3 +1,28 @@
 <div class="top-bar">
-    
+    <div class="log-field">
+        <?php if (!isset($_SESSION['user'])) {
+        ?>
+            <div>
+                <div>
+                    <a href="index.php?id=signup"><button>Đăng ký</button></a>
+                </div>
+                <div>
+                    <a href="index.php?id=signin"><button>Đăng nhập</button></a>
+                </div>
+            </div>
+        <?php
+        } else if (isset($_SESSION['user'])) {
+            $data = $_SESSION['user']; ?>
+            <div>
+                <div class="dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <img src="images/icon-user.png" alt="anhdaidien" class="user-icon">
+                    <?php echo $data['hoten'] ?>
+                </div>
+                <div class="dropdown-menu dropdown-menu-right">
+                    <a class="dropdown-item" href="index.php?id=quanlytaikhoan">Quản lý tài khoản</a>
+                    <a class="dropdown-item" href="index.php?id=out">Đăng xuất</a>
+                </div>
+            </div>
+        <?php } ?>
+    </div>
 </div>
--- index.php
+++ index.php
@@ -20,64 +20,58 @@ if (isset($_GET['id'])) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="css/style.css" type="text/css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"></script> 
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script> 
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <script src="js/myjs.js" type="text/javascript"></script>
-    <style>
-        <?php //include "./css/style.css" 
-        ?>
-    </style>
-    <script type="text/javascript">
-        <?php include "./js/myjs.js" ?>
-    </script>
+
     <title>Ai shop</title>
 </head>
 
 <body class="container-fluid">
-    <div id="overlay">
-        <div><img src="images/loading.gif" width="64px" height="64px" /></div>
-    </div>
-    <?php //include "include/topbar.php"; ?>
-    <?php include "include/header.php"; ?>
-    <?php include "include/menu.php"; ?>
-    <div class="container" id="data">
-    </div>
-    <div id="main" id="main">
-        <?php
-        if (isset($_GET['id'])) {
-            $id = $_GET['id'];
-
-            if ($id == 'chitiet-sp') {
-                include "include/chitiet-sp.php";
-            }
-            if ($id == 'quanlytaikhoan') {
-                include "include/quanlytaikhoan.php";
-            }
-            if ($id == 'signin') {
-                include "include/sign-in.php";
-            }
-            if ($id == 'signup') {
-                include "include/sign-up.php";
-            }
-            if ($id == 'shop-cart') {
-                include "include/shop-cart.php";
-            }
-        } else 
-                if (isset($_GET['search'])) {
-            include "include/hienthi-sp.php";
-        } else {
-            include "include/banner.php";
-            include "include/sp-tieubieu.php";
-        }
-        ?>
-        <div class="clear">
+    <div class="container">
+        <div id="overlay">
+            <div><img src="images/loading.gif" width="64px" height="64px" /></div>
         </div>
-    </div>
+        <?php include "include/topbar.php"; ?>
+        <?php include "include/header.php"; ?>
+        <?php include "include/menu.php"; ?>
+        <div class="conteiner" id="data">
+        </div>
+        <div id="main" id="main">
+            <?php
+            if (isset($_GET['id'])) {
+                $id = $_GET['id'];
 
-    <?php include "include/footer.php"; ?>
+                if ($id == 'chitiet-sp') {
+                    include "include/chitiet-sp.php";
+                }
+                if ($id == 'quanlytaikhoan') {
+                    include "include/quanlytaikhoan.php";
+                }
+                if ($id == 'signin') {
+                    include "include/sign-in.php";
+                }
+                if ($id == 'signup') {
+                    include "include/sign-up.php";
+                }
+                if ($id == 'shop-cart') {
+                    include "include/shop-cart.php";
+                }
+            } else 
+                if (isset($_GET['search'])) {
+                include "include/hienthi-sp.php";
+            } else {
+                include "include/banner.php";
+                include "include/sp-tieubieu.php";
+            }
+            ?>
+            <div class="clear">
+            </div>
+        </div>
+
+        <?php include "include/footer.php"; ?>
     </div>
 </body>
 
